af0a429e40e2078857076d94f6b877524e5d2d0c
rajeevprasanna <rajeevprasanna@gmail.com>
2014-04-02 13:16:27 +0530

    updating code which is retrieving all files in the root folder

--- GoogleDriveIntegration/GoogleDriveIntegration/GoogleDriveTableViewController.m
+++ GoogleDriveIntegration/GoogleDriveIntegration/GoogleDriveTableViewController.m
@@ -13,8 +13,8 @@
 
 static NSString *const scopes = @"https://www.googleapis.com/auth/drive.file";
 static NSString *const keychainItemName = @"gDrive";
-static NSString *const clientId = @"516271788433-npfpoi6el7noilv874asmmjfupaaqljqc.apps.googleusercontent.com";
-static NSString *const clientSecret = @"yCOifH7LyIHrZHPAWwksqQxl2";
+static NSString *const clientId = @"516271788433-npfpoi6el7noilv874ammjfupaaqljqc.apps.googleusercontent.com";
+static NSString *const clientSecret = @"yCOifH7LyIHrZHPAWwkqQxl2";
 
 
 @interface GoogleDriveTableViewController ()
@@ -31,6 +31,9 @@ static NSString *const clientSecret = @"yCOifH7LyIHrZHPAWwksqQxl2";
 
 @implementation GoogleDriveTableViewController
 
+@synthesize driveFiles = _driveFiles;
+@synthesize isAuthorized = _isAuthorized;
+
 - (id)initWithStyle:(UITableViewStyle)style
 {
     self = [super initWithStyle:style];
@@ -43,6 +46,7 @@ static NSString *const clientSecret = @"yCOifH7LyIHrZHPAWwksqQxl2";
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self driveService];
     
     GTMOAuth2Authentication *auth =
     [GTMOAuth2ViewControllerTouch authForGoogleFromKeychainForName:keychainItemName clientID:clientId clientSecret:clientSecret];
@@ -52,6 +56,16 @@ static NSString *const clientSecret = @"yCOifH7LyIHrZHPAWwksqQxl2";
      }else{
          [self authorizeUser];
      }
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    // Sort Drive Files by modified date (descending order).
+    [self.driveFiles sortUsingComparator:^NSComparisonResult(GTLDriveFile *lhs,
+                                                             GTLDriveFile *rhs) {
+        return [rhs.modifiedDate.date compare:lhs.modifiedDate.date];
+    }];
+    [self.tableView reloadData];
 }
 
 - (void)authorizeUser {
@@ -89,7 +103,63 @@ static NSString *const clientSecret = @"yCOifH7LyIHrZHPAWwksqQxl2";
 }
 
 - (void)loadDriveFiles {
-    NSLog(@"Going to load files from Google drive.....");
+    
+    GTLQueryDrive *query = [GTLQueryDrive queryForFilesList];
+    query.q = @"'root' in parents and trashed=false and hidden=false";
+    
+    
+    UIAlertView *alert = [self showLoadingMessageWithTitle:@"Loading files" delegate:self];
+    self.driveService.shouldFetchNextPages = YES;
+    [self.driveService executeQuery:query completionHandler:^(GTLServiceTicket *ticket,
+                                                              GTLDriveFileList *files,
+                                                              NSError *error) {
+        
+        [alert dismissWithClickedButtonIndex:0 animated:YES];
+        
+        if (error == nil) {
+            if (self.driveFiles == nil) {
+                NSLog(@"NS dirve files are null");
+                self.driveFiles = [[NSMutableArray alloc] init];
+            }
+            
+            //Refer here to donwload a file : https://developers.google.com/drive/v2/reference/files/get
+            GTLDriveFile *file =  [files objectAtIndexedSubscript:0];
+            NSLog(@"file Title => %@", file.title);
+            NSLog(@"file type => %@", file.mimeType);
+            
+            //if user is in root directly, don't display back button
+            //file id => ("GTLDriveParentReference 0x8e549f0: {id:\"0APagr-5pqYunUk9PVA\" kind:\"drive#parentReference\" isRoot:1}" )
+            
+            NSLog(@"file id => %@", file.parents);
+            NSLog(@"file download url => %@", file.downloadUrl);
+            
+            [self.driveFiles removeAllObjects];
+            [self.driveFiles addObjectsFromArray:files.items];
+            [self.tableView reloadData];
+        } else {
+            NSLog(@"An error occurred: %@", error);
+            [self showErrorMessageWithTitle:@"Unable to load files"
+                                               message:[error description]
+                                              delegate:self];
+        }
+    }];
+}
+
+- (GTLServiceDrive *)driveService {
+    static GTLServiceDrive *service = nil;
+    
+    if (!service) {
+        service = [[GTLServiceDrive alloc] init];
+        
+        // Have the service object set tickets to fetch consecutive pages
+        // of the feed so we do not need to manually fetch them.
+        service.shouldFetchNextPages = YES;
+        
+        // Have the service object set tickets to retry temporary error conditions
+        // automatically.
+        service.retryEnabled = YES;
+    }
+    return service;
 }
 
 - (void)didReceiveMemoryWarning
@@ -106,18 +176,42 @@ static NSString *const clientSecret = @"yCOifH7LyIHrZHPAWwksqQxl2";
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-     return 3;
+  return self.driveFiles.count;
 }
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     
-    // Configure the cell...
-    UILabel *label = (UILabel *)[cell viewWithTag:100];
-    label.text = [NSString stringWithFormat:@"%i", indexPath.row];
+    GTLDriveFile *file = [self.driveFiles objectAtIndex:indexPath.row];
+    cell.textLabel.text = file.title;
     return cell;
 }
 
+-(UIAlertView *)showLoadingMessageWithTitle:(NSString *)title
+delegate:(id)delegate {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:@""
+                                                   delegate:self
+                                          cancelButtonTitle:nil
+                                          otherButtonTitles:nil];
+    UIActivityIndicatorView *progress=
+    [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(125, 50, 30, 30)];
+    progress.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
+    [alert addSubview:progress];
+    [progress startAnimating];
+    [alert show];
+    return alert;
+}
+
+- (void)showErrorMessageWithTitle:(NSString *)title
+                          message:(NSString*)message
+                         delegate:(id)delegate {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:self
+                                          cancelButtonTitle:@"Dismiss"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
 @end
